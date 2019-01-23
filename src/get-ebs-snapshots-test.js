ec24c8e0a13581ef9f2f19c101204b32e87dda7d
Travis Dahlin <trdahlin@gmail.com>
2019-01-22 23:39:35 -0800

    Add OwnerIds filter.

--- src/get-ebs-snapshots-test.js
+++ src/get-ebs-snapshots-test.js
@@ -2,6 +2,10 @@ import { snapshotDateFilter } from "./get-ebs-snapshots";
 
 (async function test() {
   const expireDate = "16 Oct 2018";
-  const snapshots = await snapshotDateFilter(expireDate, "us-west-2");
+  const snapshots = await snapshotDateFilter(
+    expireDate,
+    "217880599903",
+    "us-west-2"
+  );
   console.log(snapshots.slice(0, 10));
 })();
--- src/get-ebs-snapshots.js
+++ src/get-ebs-snapshots.js
@@ -6,9 +6,11 @@ import AWS from "aws-sdk";
  * @param {string} region - The AWS region to query.
  * @returns {array} Array of objects.
  */
-export async function snapshotDateFilter(expireDate, region) {
+export async function snapshotDateFilter(expireDate, ownerId, region) {
   const ec2 = new AWS.EC2({ region: region });
-  const snapshots = await ec2.describeSnapshots().promise();
+  const snapshots = await ec2
+    .describeSnapshots({ OwnerIds: [ownerId] })
+    .promise();
   return snapshots.Snapshots.filter(
     snapshot => Date.parse(snapshot.StartTime) < Date.parse(expireDate)
   );
