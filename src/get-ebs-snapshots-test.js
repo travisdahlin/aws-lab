fda3ce53308264799a57dbebdc2149a4b049a21e
Travis Dahlin <trdahlin@gmail.com>
2019-01-23 00:29:43 -0800

    Update gitignore.

--- src/get-ebs-snapshots-test.js
+++ src/get-ebs-snapshots-test.js
@@ -2,10 +2,6 @@ import { snapshotDateFilter } from "./get-ebs-snapshots";
 
 (async function test() {
   const expireDate = "22 Jan 2019";
-  const snapshots = await snapshotDateFilter(
-    expireDate,
-    ["704909965980"],
-    "us-west-2"
-  );
+  const snapshots = await snapshotDateFilter(expireDate, [], "us-west-2");
   console.log(snapshots.slice(0, 10));
 })();
